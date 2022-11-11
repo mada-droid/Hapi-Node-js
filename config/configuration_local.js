a4c96c71319b3b76ea935fc4643b76362b646bf0
mada-droid <75455554+mada-droid@users.noreply.github.com>
2022-11-11 09:31:48 +0100

    Update configuration_local.js

--- config/configuration_local.js
+++ config/configuration_local.js
@@ -36,7 +36,7 @@ module.exports = {
     connection: {
       database: 'objection_tutorial',
       user:     'postgres',
-      password: 'luffY321@'
+      password: '*********'
     },
     pool: {
       min: 2,
